079f441fe3b05d88f59c769d19e358fca588af5a
nigelsuarez <suareznigel2005@gmail.com>
2025-10-28 15:17:41 +0800

    Lab2: update files and add new work

--- Lab2.py
+++ Lab2.py
@@ -1,1 +1,21 @@
-print("Test")
+def calculateBMI(height, weight):
+    print("Height = " + str(height) )
+    print("Weight = " + str(weight) )
+
+def BMIcalculator(height, weight):
+    bmi = weight/(height*height)
+    print("BMI: "+ str(bmi))
+
+    if bmi < 18.5:
+        print("BMI Range: Under Weight")
+    elif bmi < 25.0:
+        print("BMI Range: Normal Weight")
+    elif bmi > 25.0:
+        print("BMI Range: Over Weight")
+
+    return bmi
+
+
+calculateBMI(weight=57, height=1.73)
+
+BMIcalculator(weight=57,height=1.73)
